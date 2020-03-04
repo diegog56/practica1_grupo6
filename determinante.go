f53320621ce29de693de42f135319149f9e053c9
Diego <gomezefdiego@gmail.com>
2020-03-04 09:27:46 -0600

    Multiplicacion y division terminadas

--- determinante.go
+++ determinante.go
@@ -35,10 +35,19 @@ func potenciaCubo(num float64) {
 	fmt.Println(num, "elevado a 3 es:", math.Pow(num, 3))
 }
 
+func mult(factor1 int,factor2 int) int{
+	return factor1*factor2
+}
+
+func div(dividend int,divider int) int{
+	return dividend/divider
+}
 
 func main() {
     determinante2x2(4,2,2,2)
     absolute(-59854)
     potenciaCubo(4)
-	  raizCubica(64)
+    raizCubica(64)
+    fmt.Println(mult(4,2))
+    fmt.Println(div(4,3))
 }
