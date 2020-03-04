d619a127aa5aafa6c8c5433b4b3d3d428108a738
Brandon Pedroza <pato094@live.com>
2020-03-04 11:16:26 -0600

    agregando validacion para mostrar que los numeros negativos no son primos

--- validarPrimo.go
+++ validarPrimo.go
@@ -8,6 +8,10 @@ import ("fmt"
 
 func validarPrimo(numero int) {
     contador:= 0 
+    if numero <0{
+        fmt.Println(strconv.Itoa(numero)+ " Es numero negativo. Por tanto no es primo")
+        return
+    }
     for i := 1; i <= numero; i++{
         if numero % i  == 0{
             contador++
@@ -23,6 +27,8 @@ func validarPrimo(numero int) {
 func main() {
     validarPrimo(65)
     validarPrimo(97)
+    validarPrimo(-24)
+    validarPrimo(110)
 }
 
 
